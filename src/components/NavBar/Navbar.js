d82cffb68b3d0a4d7ab8f1f43bdad0390fc8cbda
Sameer <mohammedsameer20662@gmail.com>
2024-09-20 08:09:17 +0530

    changes done

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -27,8 +27,8 @@ const Navbar = () => {
         <div className="navMenu" style={{ display: showMenu? 'flex': 'none'}}>
           <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
           <Link activeClass='active' to='skills' spy={true} smooth={true} offset={10} duration={500} className="listItem" onClick={() => setShowMenu (false)}>About</Link>
-          <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=> setShowMenu(false)}>Portfolio</Link>
-          <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-40} duration={500} className="listItem" onClick={()=> setShowMenu(false)}>Clients</Link>
+          <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=> setShowMenu(false)}>Skills</Link>
+          <Link activeClass='active' to='clients' spy={true} smooth={true} offset={-40} duration={500} className="listItem" onClick={()=> setShowMenu(false)}>Experience</Link>
         </div>
       </nav>
   )

c0edb446ed600a74c60fb7a852acb5bfd0b62bfe
Sameer <mohammedsameer20662@gmail.com>
2025-04-30 10:23:24 +0530

    updates added

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Intro.css'
 import bg from '../../assets/image.png';
+import sam from '../../assets/sam.png';
 import {Link} from 'react-scroll';
 import btnImg from '../../assets/hireme.png';
+import resume from '../../assets/Sameer_Over_all.pdf';
+import download from '../../assets/download.webp';
 const Intro = () => {
   return (
     <section id='intro'>
@@ -10,9 +13,20 @@ const Intro = () => {
             <span className="hello">Hello</span>
             <span className="introText">I'm <span className="introName">Mohammed Sameer</span><br />Full Stack Developer</span>
             <p className="introPara">I am passionate about building innovative solutions and applying <br />machine learning techniques to solve real-world problems.<br /> Currently, I'm a student honing my skills and working on exciting projects.</p>
-            <Link to="contact" smooth={true} duration={500}><button className='btn'><img src={btnImg} alt='hire' className='btnImg' />Hire Me</button></Link>
+            <div className="btnContainer">
+              <Link to="contact" smooth={true} duration={500}>
+                <button className='btn'>
+                  <img src={btnImg} alt='hire' className='btnImg' />Hire Me
+                </button>
+              </Link>
+              <a href={resume} download="Mohammed_Sameer_Resume.pdf">
+                <button className='btn'>
+                  <img src={download} alt='resume' className='btnImg' /> Resume
+                </button>
+              </a>
+          </div>
         </div>
-        <img src={bg} alt='Profile' className='bg'/>
+        <img src={sam} alt='Profile' className='bg'/>
     </section>
   )
 }
